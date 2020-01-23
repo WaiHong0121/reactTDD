10c4c489b2a9f7f9f53abaaf5b33104f058ad6d5
WH <edwinhong_waihong@hotmail.com>
2020-01-23 15:00:46 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["codingitwrong"],
+  parser: "babel-eslint",
+  plugins: ["cypress", "jest", "react"],
+  env: { browser: true, "jest/globals": true, "cypress/globals": true }
+};
---
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -0,0 +1,13 @@
+describe("adding a restaurant", () => {
+  it("displays the restaurant in the list", () => {
+    const restaurantName = "Sushi Place";
+
+    cy.visit("http://localhost:1234");
+
+    cy.get('[data-test="newRestaurantName"]').should("not.exist");
+    cy.get('[data-test="addRestaurantButton"]').click();
+    cy.get('[data-test="newRestaurantName"]').type("Sushi Place");
+    cy.get('[data-test="saveNewRestaurantButton"]').click();
+    cy.contains("Sushi Place");
+  });
+});
