48ef688c950eaec073caf12a136c32852740c662
WH <edwinhong_waihong@hotmail.com>
2020-01-26 21:36:17 +0800

    update8

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -32564,7 +32564,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37953" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45045" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
